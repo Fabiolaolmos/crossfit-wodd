aa30846bfd202c1e00ee44e4312a384eeda96147
Fabiolaolmos <138613359+Fabiolaolmos@users.noreply.github.com>
2023-07-26 20:45:46 +0000

    segunda parte de la aplicaciòn funcionando

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 // In src/index.js
 const express = require("express");
-// *** REMOVE ***
-const v1Router = require("./v1/routes");
+
 // *** ADD ***
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// *** REMOVE ***
-app.use("/api/v1", v1Router);
+
 
 // *** ADD ***
 app.use("/api/v1/workouts", v1WorkoutRouter);
