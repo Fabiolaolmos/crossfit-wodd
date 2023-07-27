0a7af6bdf8493d46ed41d24bb0006d6467111bc2
Fabiolaolmos <138613359+Fabiolaolmos@users.noreply.github.com>
2023-07-27 05:21:23 +0000

    Integraciòn de las rutas para nuestro registro

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,6 +1,8 @@
 // In src/v1/routes/workoutRoutes.js
 const express = require("express");
 const workoutController = require("../../controllers/workoutController");
+// *** ADD ***
+const recordController = require("../../controllers/recordController");
 
 const router = express.Router();
 
@@ -8,10 +10,13 @@ router.get("/", workoutController.getAllWorkouts);
 
 router.get("/:workoutId", workoutController.getOneWorkout);
 
+// *** ADD ***
+router.get("/:workoutId/records", recordController.getRecordForWorkout);
+
 router.post("/", workoutController.createNewWorkout);
 
 router.patch("/:workoutId", workoutController.updateOneWorkout);
 
 router.delete("/:workoutId", workoutController.deleteOneWorkout);
 
-module.exports = router;
+module.exports = router
