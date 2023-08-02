b25fc599098ac33d922123cb6ab69d108d62fb1e
Fabiolaolmos <138613359+Fabiolaolmos@users.noreply.github.com>
2023-08-02 00:21:14 +0000

    funcionando v1

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,16 +1,57 @@
-// In src/v1/routes/workoutRoutes.js
 const express = require("express");
 const workoutController = require("../../controllers/workoutController");
-// *** ADD ***
 const recordController = require("../../controllers/recordController");
 
 const router = express.Router();
 
+/**
+ * @openapi
+ * /api/v1/workouts:
+ *   get:
+ *     tags:
+ *       - Workouts
+ *     parameters:
+ *       - in: query
+ *         name: mode
+ *         schema:
+ *           type: string
+ *         description: The mode of a workout
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array 
+ *                   items: 
+ *                     $ref: "#/components/schemas/Workout"
+ *       5XX:
+ *         description: FAILED
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status: 
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string 
+ *                       example: "Some error message"
+ */
 router.get("/", workoutController.getAllWorkouts);
 
 router.get("/:workoutId", workoutController.getOneWorkout);
 
-// *** ADD ***
 router.get("/:workoutId/records", recordController.getRecordForWorkout);
 
 router.post("/", workoutController.createNewWorkout);
@@ -19,4 +60,6 @@ router.patch("/:workoutId", workoutController.updateOneWorkout);
 
 router.delete("/:workoutId", workoutController.deleteOneWorkout);
 
-module.exports = router
+
+
+module.exports = router;
